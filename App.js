63cd60f90e7b358ef089e540f1265e1653b24445
ahmed chaouch <chaouchhedi1@gmail.com>
2023-05-10 11:52:23 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>welcam to react js</p>
+      <Form.Group controlId="formFile" className="mb-3">
+        <Form.Label>hello</Form.Label>
+        <Form.Control type="file" />
+      </Form.Group>
+      <Form.Group controlId="formFileMultiple" className="mb-3">
+        <Form.Label>mote de passe</Form.Label>
+        <Form.Control type="file" multiple />
+      </Form.Group>
+      <Form.Group controlId="formFileDisabled" className="mb-3">
+        <Form.Label>className</Form.Label>
+        <Form.Control type="file" disabled />
+      </Form.Group>
+      <Form.Group controlId="formFileSm" className="mb-3">
+        <Form.Label>Small file input example</Form.Label>
+        <Form.Control type="file" size="sm" />
+      </Form.Group>
+      <Form.Group controlId="formFileLg" className="mb-3">
+        <Form.Label>Large file input example</Form.Label>
+        <Form.Control type="file" size="lg" />
+      </Form.Group>
+    /</div>
+  );
+}
